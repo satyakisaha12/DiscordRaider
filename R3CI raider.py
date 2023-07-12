321b8a36fd5e455f1f113213ab38c26428fa0292
_R3CI_ <139361332+R3CI@users.noreply.github.com>
2023-07-12 19:34:57 +0200

    - Addes a leaver

--- R3CI raider.py
+++ R3CI raider.py
@@ -11,27 +11,74 @@ print('''
                                                                                             
 ''')
 
-Token = input("token ONLY ONE: ")
-channelID = input("Channel ID: ")
-MessageContent = input("Message: ")
-RepeatCount = int(input("How many times do you want to send the message? "))
-Delay = int(input("What should the delay be (in seconds)? "))
+print('''
+1. Joiner
+2. Leaver
+3. Spammer
+''')
 
-payload = {
-    'content': MessageContent
-}
+Chose = int(input("Type the number of the option you want: "))
 
-header = {
-    'authorization': Token
-}
+if Chose == 1:
+    Token = input("token: ")
+    Invite = input("invite: ")
 
-for i in range(RepeatCount):
-    url = f"https://discord.com/api/v9/channels/{channelID}/messages"
-    time.sleep(Delay)
+    header = {
+        'Authorization': Token
+    }
+    
+    url = f"https://discord.com/api/v9/invites/{Invite}"
 
-    response = requests.post(url, data=payload, headers=header)
-
+    response = requests.post(url, headers=header)
+    
     if response.status_code == 200:
-        print(f"Message {i+1} sent successfully.")
+        print("Successfully joined.")
     else:
-        print(f"Error occurred while sending message {i+1}. Status code:", response.status_code)
+        print("Error occurred while joining the server. Status code:", response.status_code)
+
+# LEAVER
+
+if Chose == 2: 
+    Token = input("token: ")
+    GuildID = input("Guild ID: ")
+
+    header = {
+        'Authorization': Token
+    }
+
+    url = f"https://discord.com/api/v9/users/@me/guilds/{GuildID}"
+
+    response = requests.delete(url, headers=header)
+
+    if response.status_code == 204:
+        print("Left the server successfully.")
+    else:
+        print("Error occurred while leaving the server. Status code:", response.status_code)
+
+# SPAMMER
+
+if Chose == 3:
+    Token = input("token: ")
+    channelID = input("Channel ID: ")
+    MessageContent = input("Message: ")
+    RepeatCount = int(input("Repeat (how many times): "))
+    Delay = int(input("Delay (in seconds): "))
+
+    payload = {
+        'content': MessageContent
+    }
+
+    header = {
+        'Authorization': Token
+    }
+
+    for i in range(RepeatCount):
+        url = f"https://discord.com/api/v9/channels/{channelID}/messages"
+        time.sleep(Delay)
+
+        response = requests.post(url, json=payload, headers=header)
+
+        if response.status_code == 200:
+            print(f"Message {i+1} sent successfully.")
+        else:
+            print(f"Error occurred while sending message {i+1}. Status code:", response.status_code)
