0d5263fd54f4b46a7555fddc8c0d424175d9b909
_R3CI_ <139361332+R3CI@users.noreply.github.com>
2023-07-16 18:00:55 +0200

    frogot to add lastert src

--- Mooner.py
+++ Mooner.py
@@ -4,6 +4,15 @@ import threading
 import shutil
 import os
 
+# Colours
+# Purple light \033[95m
+# Purple dark \033[38;5;53m
+# Yellow \u001b[33m
+# Red \u001b[31m
+
+# Error codes
+# 403 Locked
+
 # Making data folder
 if not os.path.exists("data"):
     os.makedirs("data")
@@ -16,12 +25,20 @@ if not os.path.exists(file_path):
     with open(file_path, 'w'):
         pass
 
+# Making webhooks.txt
+file_pathWB = os.path.join("data", "webhooks.txt")
+if not os.path.exists(file_pathWB):
+    print("Webhooks file created")
+    with open(file_pathWB, 'w'):
+        pass
+
 while True:
     # Clearing the console
     os.system('cls' if os.name == 'nt' else 'clear')
 
     # File path to the tokens
     file_path = "data/tokens.txt"
+    file_pathWB = "data/webhooks.txt"
 
     # Counting tokens
     def count_lines(file_path):
@@ -32,26 +49,29 @@ while True:
         return line_count
 
     # Print mooner logo
-    moon_logo = '''
-    __  __                             
-    |  \/  | ___   ___  _ __   ___ _ __ 
-    | |\/| |/ _ \ / _ \| '_ \ / _ \ '__|
-    | |  | | (_) | (_) | | | |  __/ |   
-    |_|  |_|\___/ \___/|_| |_|\___|_|   
+    moon_logo = '''\033[95m
+    ███╗   ███╗ ██████╗  ██████╗ ███╗   ██╗███████╗██████╗ 
+    ████╗ ████║██╔═══██╗██╔═══██╗████╗  ██║██╔════╝██╔══██╗
+    ██╔████╔██║██║   ██║██║   ██║██╔██╗ ██║█████╗  ██████╔╝
+    ██║╚██╔╝██║██║   ██║██║   ██║██║╚██╗██║██╔══╝  ██╔══██╗
+    ██║ ╚═╝ ██║╚██████╔╝╚██████╔╝██║ ╚████║███████╗██║  ██║
+    ╚═╝     ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝  
                                         
-    Made by _r3ci_  youtube.com/@_R3CI dsc.gg/r3ci
+    Made by _r3ci_  youtube.com/@_R3CI dsc.gg/r3ci\033[0m
     '''
 
     # Token count print
-    token_count_text = "You have currently loaded {0} tokens".format(count_lines(file_path))
+    token_count_text = "            \033[38;5;53mYou have currently loaded {0} tokens\033[38;5;53m".format(count_lines(file_path))
 
     # Options print
     options = '''
-    1. Joiner IN WORK           6. Soon
-    2. Leaver                   7. Soon
-    3. Spammer                  8. Soon
-    4. DM Spammer IN WORK       9. Soon
-    5. Webhook spammer          10. Soon
+    
+                | \033[95m <1> \033[95m \033[38;5;53m Joiner  \033[38;5;53m         |
+                | \033[95m <2> \033[95m \033[38;5;53m Leaver \033[38;5;53m          |
+                | \033[95m <3> \033[95m \033[38;5;53m Spammer \033[38;5;53m         |
+                | \033[95m <4> \033[95m \033[38;5;53m Reactor \033[38;5;53m         |
+                | \033[95m <5> \033[95m \033[38;5;53m Webhook spammer \033[38;5;53m |
+    
     '''
 
     # Middle text stuff
@@ -75,7 +95,10 @@ while True:
             header = {
                 'Authorization': token
             }
-
+            
+            payload = {
+                'session_id': "cbdc916cbb4c6d6dbc2f5f9f36451e7f"
+            }
             url = f"https://discord.com/api/v9/invites/{invite}"
 
             response = requests.post(url, headers=header)
@@ -167,59 +190,55 @@ while True:
         for thread in threads:
             thread.join()
 
-    # DM SPAMMER
+    # Reactor
     if choice == 4:
         with open(file_path, "r") as file:
             tokens = file.read().splitlines()
 
         channel_id = input("Channel ID: ")
-        message_content = input("Message: ")
-        repeat_count = int(input("Repeat (how many times): "))
-        delay = int(input("Delay (in seconds): "))
+        message_id = input("Message ID: ")
+        emoji = input("Emoji: ")
 
-        payload = {
-            'content': message_content
-        }
-
-        def send_dm(token):
-            header = {
+        for token in tokens:
+            headers = {
                 'Authorization': token
             }
 
-            for i in range(repeat_count):
-                url = f"https://discord.com/api/v9/channels/{channel_id}/messages"
-                time.sleep(delay)
+            url = f"https://discord.com/api/v9/channels/{channel_id}/messages/{message_id}/reactions/{emoji}/@me"
+            response = requests.put(url, headers=headers)
 
-                response = requests.post(url, json=payload, headers=header)
+            if response.status_code == 204:
+                print("Reaction added successfully with token:", token[:-5] + "**********")
+            else:
+                print("Error occurred while adding the reaction with token:", token[:-5] + "**********", "Status code:", response.status_code)
+                
+    # Webhook spammer
+    if choice == 5:
+        with open(file_pathWB, "r") as file:
+            webhooks = file.read().splitlines()
+        
+        message = input("Message: ")
+        repeat_count = int(input("Repeat count: "))
+        
+        payload = {'content': message}
 
-                if response.status_code == 200:
-                    print(f"Message {i + 1} sent successfully with token:", token[:-5] + "**********")
+        def send_webhook_message(webhook_url):
+            for _ in range(repeat_count):
+                response = requests.post(webhook_url, json=payload)
+            
+                if response.status_code == 204:
+                    print("Message sent successfully to", webhook_url[:-5] + "**")
                 else:
-                    print(f"Error occurred while sending message {i + 1} with token:", token[:-5] + "**********", "Status code:", response.status_code)
-
+                    print("Failed to send message to", webhook_url[:-5] + "**", "Error:", response.text)
+        
         threads = []
-        for token in tokens:
-            thread = threading.Thread(target=send_dm, args=(token,))
+        for webhook_url in webhooks:
+            thread = threading.Thread(target=send_webhook_message, args=(webhook_url,))
             thread.start()
             threads.append(thread)
-
+        
         for thread in threads:
             thread.join()
-
-    if choice == 5:
-        Webhook = input("Webhook: ")
-        repeat_count = int(input("Repeat (how many times): "))
-        message_content = input("Message: ")
-
-        payload = {
-            "content": message_content
-        }
-        for i in range(repeat_count):
-            response = requests.post(Webhook, json=payload)
-
-        if response.status_code == 204:
-            print("Message sent successfully!")
-        else:
-            print("Failed to send the message. Status code: ", response.status_code)
-    print("Operation completed. Returning in 2s")
-    time.sleep(2)
+    
+    print("Operation completed. Returning in 5s")
+    time.sleep(5)
