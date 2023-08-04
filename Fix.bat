b4089a3aecc13bc04ffd90208146f48ea549ccf7
_R3CI_ <139361332+R3CI@users.noreply.github.com>
2023-08-04 20:37:17 +0200

    Add files via upload

---
+++ Fix.bat
@@ -0,0 +1,8 @@
+python.exe -m pip install --upgrade pip
+pip install requests
+pip install os
+pip install webbrowser
+pip install ctypes
+pip install logging
+pip install colorama
+pip install pystyle
