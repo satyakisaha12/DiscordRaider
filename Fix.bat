10c7390887e58c38ea7b0a7d8fa369ad34927e59
_R3CI_ <139361332+R3CI@users.noreply.github.com>
2023-08-14 23:42:02 +0200

    Update Fix.bat

--- Fix.bat
+++ Fix.bat
@@ -1,8 +1,5 @@
 python.exe -m pip install --upgrade pip
 pip install requests
-pip install os
-pip install webbrowser
-pip install ctypes
 pip install logging
 pip install colorama
 pip install pystyle
